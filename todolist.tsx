61e1c0793b206c7bfd278bef3f2c62e7c6a54ded
원준엽 <wnsduq2178@gmail.com>
2023-01-27 23:29:41 +0900

    JSdoc

--- todolist.tsx
+++ todolist.tsx
@@ -3,19 +3,19 @@
 /**
  * 할일
  * @typedef {Object} Todo
- * @property {number(required)} id - 할일 id
- * @property {string(required)} content - 할일 내용
- * @property {boolean(required)} completed - 할일 완료 여부
- * @property {string(required)} category -카테고리
- * @property {string(optional)} tag - 태그
+ * @property {int(11)} id - 할일 id
+ * @property {varchar(256)} contents - 할일 내용
+ * @property {boolean} completed - 할일 완료 여부
+ * @property {varchar(20)} category -카테고리
+ * @property {JSON} tags - 태그
  */
 
 /**
  * 할일 목록
- * @type {Todo[]}
+ * @type {Todos(JSON)}
  */
 const todos = [
-    { id: 1, content: 'HTML', completed: false, category: 'subject' },
-    { id: 2, content: 'CSS', completed: true, category: 'subject' },
-    { id: 3, content: 'Javascript', completed: false, category: 'subject', tag:'languege' },
+    { id: 1, content: 'HTML', completed: false, contents:'', category: 'subject' },
+    { id: 2, content: 'CSS', completed: true, contents:'', category: 'subject' },
+    { id: 3, content: 'Javascript', completed: false, contents:'', category: 'subject', tags:'languege' },
   ];
